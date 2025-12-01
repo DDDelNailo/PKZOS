248d5cac2840fcf16c2f50686a79a32aef520b45
DDDelNailo <danilodantas7002@gmail.com>
2025-12-01 13:31:56 -0300

    Fixes #1: Added terminal input history

--- apps/terminal.py
+++ apps/terminal.py
@@ -4,7 +4,7 @@ from pygame.event import Event
 from app_base import BaseApp
 
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
 
 if TYPE_CHECKING:
     from kernel import Kernel
@@ -14,6 +14,9 @@ class TerminalApp(BaseApp):
     def __init__(self, kernel: "Kernel", namespace: str) -> None:
         super().__init__(kernel, namespace, title="Terminal")
         self.lines: list[str] = []
+        self.inp_history: list[str] = []
+        self.inp_history_idx: Optional[int] = None
+        self.inp_history_temp: str = ""
         self.current: str = ""
         self.current_prefix: str = "[root@pkzos /]$"
 
@@ -26,6 +29,8 @@ class TerminalApp(BaseApp):
 
     def send(self) -> None:
         text = self.current.strip()
+        self.inp_history.append(text)
+        self.inp_history_idx = None
         self.lines.append(self.current_prefix + " " + text)
         self.current = ""
         if len(self.lines) > self.max_lines:
@@ -42,6 +47,7 @@ class TerminalApp(BaseApp):
 
         if event.unicode and len(event.unicode) == 1 and event.unicode.isprintable():
             self.current += event.unicode
+            self.inp_history_idx = None
             return
 
         match event.key:
@@ -49,6 +55,26 @@ class TerminalApp(BaseApp):
                 self.current = self.current[:-1]
             case pygame.K_RETURN | pygame.K_KP_ENTER:
                 self.send()
+            case pygame.K_UP:
+                if self.inp_history_idx is None:
+                    self.inp_history_idx = len(self.inp_history) - 1
+                    if self.current:
+                        self.inp_history_temp = self.current
+                else:
+                    self.inp_history_idx = max(self.inp_history_idx - 1, 0)
+
+                self.current = self.inp_history[self.inp_history_idx]
+            case pygame.K_DOWN:
+                if self.inp_history_idx is not None:
+                    self.inp_history_idx = min(
+                        self.inp_history_idx + 1, len(self.inp_history)
+                    )
+
+                    if self.inp_history_idx == len(self.inp_history):
+                        self.inp_history_idx = None
+                        self.current = self.inp_history_temp
+                    else:
+                        self.current = self.inp_history[self.inp_history_idx]
             case _:
                 return
 
